052e44b1dd366dd7d29e649c5de216b649154d9b
Michael <m_runyon@u.pacific.edu>
2018-03-29 15:10:54 -0700

    Made some comments

--- src/Actor.java
+++ src/Actor.java
@@ -8,10 +8,13 @@ import acm.graphics.*;
 
 public interface Actor 
 {
+	//getters
 	public int getX();
 	public int getY();
+	public GImage getSprite();
+	
+	//setters
 	public void setX(int x);
 	public void setY(int y);
 	public void setSprite(GImage sprite);
-	public GImage getSprite();
 }
