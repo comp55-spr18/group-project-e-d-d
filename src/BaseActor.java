2452d97eb42a208e9c752750cd4c86bbf3e67dcb
Michael <m_runyon@u.pacific.edu>
2018-03-29 15:09:53 -0700

    Finished the BaseActor abstract class.

--- src/BaseActor.java
+++ src/BaseActor.java
@@ -5,4 +5,15 @@ public abstract class BaseActor implements Actor {
 	private int x;
 	private int y;
 	private GImage sprite;
+	
+	//basic getters for the coordinates and for the sprite
+	public int getX() { return this.x; }
+	public int getY() { return this.y; }
+	public GImage getSprite() { return this.sprite; }
+	
+	//The consequent setters
+	public void setX(int x) { this.x = x; }
+	public void setY(int y) { this.y = y; }
+	public void setSprite(GImage sprite) { this.sprite = sprite; }
+	
 }
