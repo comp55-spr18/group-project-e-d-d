d4716d96d005f4ed67615fc5d5b89e927ca756e2
Michael <m_runyon@u.pacific.edu>
2018-03-29 14:57:50 -0700

    Added the Actor interface to the repo

---
+++ src/Actor.java
@@ -0,0 +1,17 @@
+/*
+ * This is going to be the interface for the base Actor.
+ * Interfaces can't have non-constant or non static member
+ * variables so I'm going to just use this interface to 
+ * guarantee some methods that every that implements 
+ * Actor is going to be required to have.*/
+import acm.graphics.*;
+
+public interface Actor 
+{
+	public int getX();
+	public int getY();
+	public void setX(int x);
+	public void setY(int y);
+	public void setSprite(GImage image);
+	public GImage getSprite();
+}
