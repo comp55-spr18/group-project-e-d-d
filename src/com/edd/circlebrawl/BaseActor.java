93e48942e445042a917912b387003162ae879d1f
Danilo Baledio <d_baledio@u.pacific.edu>
2018-04-01 16:54:22 -0700

    Included a timer library, timer seed, for new genRandCoord() skeleton. Debatable as to the placement of this method. Added comments for each method. Changed setUpSrite to use the genRandCoord().

--- src/com/edd/circlebrawl/BaseActor.java
+++ src/com/edd/circlebrawl/BaseActor.java
@@ -1,10 +1,13 @@
 package com.edd.circlebrawl;
 
+import java.util.Random;
+
 import acm.graphics.GImage;
 
 public abstract class BaseActor implements Actor {
 	protected GImage sprite;
 	protected CircleBrawl driver;
+	protected Random seed = new Random();
 
 	// Setters for sprite
 	public void setX(double x) {
@@ -32,23 +35,56 @@ public abstract class BaseActor implements Actor {
 		return this.sprite;
 	}
 
+	/**
+	 * Determines if Actor intersects argument Actor
+	 * 
+	 * @param anotherActor
+	 *            the Actor that we want to check if intersects current Actor
+	 * @return true/false indicating if the two Actors are intersecting
+	 */
 	public boolean collidesWith(BaseActor anotherActor) {
 		return Math.sqrt(Math.pow((anotherActor.getX() - this.getX()), 2)
 				+ Math.pow((anotherActor.getY() - this.getY()), 2)) == sprite.getWidth()
 						+ anotherActor.getSprite().getWidth();
-	} // TODO: Make function
+	}
 
+	/**
+	 * Removes the sprite from the game
+	 */
 	public void remove() {
 		driver.remove(sprite);
 	};
 
-	// public void setupSprite(String spriteFile) {
-	// sprite = new GImage(spriteFile);
-	// sprite.setLocation(x + sprite.getWidth() / 2, y + sprite.getHeight() / 2); //
-	// TODO: This is created assuming
-	// // that the x and y of actors
-	// // represent topleft corners.
-	// driver.add(sprite);
-	// }
+	/**
+	 * Initializes a GImage objec, specifically the sprite
+	 * 
+	 * @param anotherActor
+	 *            the Actor that we want to check if intersects current Actor
+	 * @return true/false indicating if the two Actors are intersecting
+	 */
+	public void setupSprite(String spriteFile) {
+		sprite = new GImage(spriteFile);
+
+		// NOTE: Bottom replaced the top after coord generator implementation
+		// sprite.setLocation(x + sprite.getWidth() / 2, y + sprite.getHeight() / 2);
+		sprite.setLocation(genRandCoord(), genRandCoord());
+
+		driver.add(sprite);
+	}
+
+	/**
+	 * Generates a coordinate that abides by the given map dimensions
+	 * 
+	 * @return a single x or y coordinate
+	 */
+	// TODO: Determine if this is where we want to place the coordinate generator,
+	// and java.util.* import
+	// TODO: Implement the coordinate generator, should consider the bottom-right
+	// most corner of sprite in calculation
+	// TODO: Should consider if there is a pre-existing object in that space prior
+	// to regenerating
+	public double genRandCoord() {
+		return 0.0;
+	}
 
 }
