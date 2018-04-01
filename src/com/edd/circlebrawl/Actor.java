cceacfc95c81dd6ee4c02fdb43b0828aba229a2b
Danilo Baledio <d_baledio@u.pacific.edu>
2018-03-31 22:42:14 -0700

    Implemented the collidesWith method within BaseActor. Removed xCoord and yCoord field redundancies. Trickled the "double" data type change from BaseActor up to Actor. Commented out setUp sprite temporarily.

--- src/com/edd/circlebrawl/Actor.java
+++ src/com/edd/circlebrawl/Actor.java
@@ -1,26 +1,32 @@
 package com.edd.circlebrawl;
+
 /*
  * This is going to be the interface for the base Actor.
  * Interfaces can't have non-constant or non static member
  * variables so I'm going to just use this interface to 
  * guarantee some methods that every that implements 
  * Actor is going to be required to have.*/
-import acm.graphics.*;
+import acm.graphics.GImage;
 
-public interface Actor 
-{
-	//getters
-	public int getX();
-	public int getY();
+public interface Actor {
+	// getters
+	public double getX();
+
+	public double getY();
+
 	public GImage getSprite();
-	
-	//setters
+
+	// setters
 	public void setX(int x);
+
 	public void setY(int y);
+
 	public void setSprite(GImage sprite);
-	
-	//other
+
+	// other
 	public boolean collidesWith(BaseActor anotherActor);
+
 	public void remove();
+
 	public void setupSprite(String spriteFile);
 }
--- src/com/edd/circlebrawl/BaseActor.java
+++ src/com/edd/circlebrawl/BaseActor.java
@@ -1,30 +1,54 @@
 package com.edd.circlebrawl;
-import acm.graphics.*;
-import acm.program.GraphicsProgram;
+
+import acm.graphics.GImage;
 
 public abstract class BaseActor implements Actor {
-	protected int x;
-	protected int y;
 	protected GImage sprite;
 	protected CircleBrawl driver;
-	
-	//basic getters for the coordinates and for the sprite
-	public int getX() { return this.x; }
-	public int getY() { return this.y; }
-	public GImage getSprite() { return this.sprite; }
-	
-	//The consequent setters
-	public void setX(int x) { this.x = x; }
-	public void setY(int y) { this.y = y; }
-	public void setSprite(GImage sprite) { this.sprite = sprite; }
-	
-	//other
-	public boolean collidesWith(BaseActor anotherActor) { return false; } // TODO: Make function
-	public void remove() { driver.remove(sprite); };
-	public void setupSprite(String spriteFile) {
-		sprite = new GImage(spriteFile);
-		sprite.setLocation(x+sprite.getWidth()/2,y+sprite.getHeight()/2); // TODO: This is created assuming that the x and y of actors represent topleft corners.
-		driver.add(sprite);
+
+	// Setters for sprite
+	public void setX(double x) {
+		this.sprite.setLocation(x, this.sprite.getY());
 	}
-	
+
+	public void setY(double y) {
+		this.sprite.setLocation(this.sprite.getX(), y);
+	}
+
+	public void setSprite(GImage sprite) {
+		this.sprite = sprite;
+	}
+
+	// Getters for sprite
+	public double getX() {
+		return this.sprite.getX();
+	}
+
+	public double getY() {
+		return this.sprite.getY();
+	}
+
+	public GImage getSprite() {
+		return this.sprite;
+	}
+
+	public boolean collidesWith(BaseActor anotherActor) {
+		return Math.sqrt(Math.pow((anotherActor.getX() - this.getX()), 2)
+				+ Math.pow((anotherActor.getY() - this.getY()), 2)) == sprite.getWidth()
+						+ anotherActor.getSprite().getWidth();
+	} // TODO: Make function
+
+	public void remove() {
+		driver.remove(sprite);
+	};
+
+	// public void setupSprite(String spriteFile) {
+	// sprite = new GImage(spriteFile);
+	// sprite.setLocation(x + sprite.getWidth() / 2, y + sprite.getHeight() / 2); //
+	// TODO: This is created assuming
+	// // that the x and y of actors
+	// // represent topleft corners.
+	// driver.add(sprite);
+	// }
+
 }
