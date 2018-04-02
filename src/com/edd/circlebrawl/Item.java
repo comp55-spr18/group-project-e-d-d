7e29b7fe0b8987bc385a13290708c7c0d33de143
Danilo Baledio <d_baledio@u.pacific.edu>
2018-04-01 17:32:29 -0700

    Just added method documentation/comments here, no code change

--- src/com/edd/circlebrawl/Item.java
+++ src/com/edd/circlebrawl/Item.java
@@ -1,35 +1,56 @@
 package com.edd.circlebrawl;
 
 public abstract class Item extends BaseActor implements Tick {
-	
+
 	protected int multiple; // multiply any stat effects by this number
 	protected int efficacy; // the strength of the item
-	
+
+	/**
+	 * Removes all instance sof the item and triggers the activation
+	 * 
+	 * @param consumer
+	 *            the actor to which the effects of item will be applied to
+	 */
 	public void consume(BaseActor consumer) {
-		// Access the item list
-		if(driver.ITEM_LIST.contains(this)) {
-			driver.ITEM_LIST.remove(this); // remove this item from the global list
+		if (driver.ITEM_LIST.contains(this)) {
+			driver.ITEM_LIST.remove(this); // remove this item from the global item list
 		}
-		
-		remove(); // remove this item from visual display of screen
+
+		remove(); // remove item from visual display of screen
 		activate(consumer); // activate effect of this item
 	}
-	
-	public abstract void activate(BaseActor consumer); // triggers an effect such as increase in size or strength
-	
+
+	// Triggers an effect such as increase in size or strength
+	public abstract void activate(BaseActor consumer);
+
+	/**
+	 * Overrides BaseActor. Detects if the argument Actor collides with the Actor of
+	 * the current class. If so, then activate the effects.
+	 * 
+	 * @param anotherActor
+	 *            the player's character
+	 * @return boolean true/false indicating if actors are intersecting
+	 */
 	@Override
 	public boolean collidesWith(BaseActor anotherActor) {
-		if(super.collidesWith(anotherActor)) {
+		if (super.collidesWith(anotherActor)) {
 			consume(anotherActor);
 			return true;
 		}
 		return false;
 	}
-	
-	protected int getFinalEfficacy() { // returns the efficacy and any modifiers one may want to make to it
-		return efficacy*multiple;
+
+	/**
+	 * Overrides BaseActor. Detects if the argument Actor collides with the Actor of
+	 * the current class. If so, then activate the effects.
+	 * 
+	 * @return the new efficacy after application of the modifier
+	 */
+	protected int getFinalEfficacy() {
+		return efficacy * multiple;
 	}
-	
+
+	// TODO: Implement tick
 	public abstract void tick();
-	
+
 }
