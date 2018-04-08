b9a10a3e9a5d88078eb402aeefd12a10e37336dd
Mike <m_runyon@u.pacific.edu>
2018-04-07 18:24:47 -0700

    Redid some menu things. Gonna change the buttons too eventually. Also want to work on combining Cricle Brawl gif with the background to make the text radiate light onto the wall behind it.

--- src/com/edd/osvaldo/MenuPane.java
+++ src/com/edd/osvaldo/MenuPane.java
@@ -17,25 +17,26 @@ public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GButton rect;
-	private GLabel title;
-	private GImage background = new GImage("com/edd/osvaldo/Brick-wallaper-For-Background-19.jpg");
+	private GImage background = new GImage("com/edd/osvaldo/brick4.jpg");
+	private GImage title = new GImage("com/edd/osvaldo/CircleBrawl.gif", program.WINDOW_WIDTH/2, program.WINDOW_HEIGHT/2);
 	
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
 		
-		try {
-		     GraphicsEnvironment ge = 
-		         GraphicsEnvironment.getLocalGraphicsEnvironment();
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("/Fonts/Sugar Death2.tff"))); //not sure if this path format is right, so fix as you need
-		} catch (IOException|FontFormatException e) {
-		     // Optionally you can have a handle  for the exception, or no try block at all
-		}
+//		try {
+//		     GraphicsEnvironment ge = 
+//		         GraphicsEnvironment.getLocalGraphicsEnvironment();
+//		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("/Fonts/Sugar Death2.tff"))); //not sure if this path format is right, so fix as you need
+//		} catch (IOException|FontFormatException e) {
+//		     // Optionally you can have a handle  for the exception, or no try block at all
+//		}
 		
-		title = new GLabel("Circle Brawl" , program.WINDOW_WIDTH/2 , program.WINDOW_HEIGHT/2);
-		title.setFont("Sugar Death2-68");
-		title.setLocation((program.WINDOW_WIDTH- title.getWidth())/2, (program.WINDOW_HEIGHT/2) - 200);
-		rect = new GButton("Single Player", (program.WINDOW_WIDTH - 200) / 2, (program.WINDOW_HEIGHT - 200) / 2, 200, 100);
+//		title = new GLabel("Circle Brawl" , program.WINDOW_WIDTH/2 , program.WINDOW_HEIGHT/2);
+//		title.setFont("Sugar Death2-68");
+//		title.setLocation((program.WINDOW_WIDTH- title.getWidth())/2, (program.WINDOW_HEIGHT/2) - 200);
+		title.setLocation((program.WINDOW_WIDTH - title.getWidth())/2, program.WINDOW_HEIGHT/2 - 200);
+		rect = new GButton("Singleplayer", (program.WINDOW_WIDTH - 200) / 2, (program.WINDOW_HEIGHT - 200) / 2, 200, 100);
 		rect.setFillColor(Color.RED);
 	}
 
