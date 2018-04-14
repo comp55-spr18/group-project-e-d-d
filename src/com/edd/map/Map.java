5483deadd761b14105522dde1380cf3813d84cc0
Michael <m_runyon@u.pacific.edu>
2018-04-14 13:41:50 -0700

    Renamed some i to rows and the j to cols.

--- src/com/edd/map/Map.java
+++ src/com/edd/map/Map.java
@@ -17,9 +17,9 @@ public class Map {
 	public GImage createImage(){
 		BufferedImage image = new BufferedImage(map.length*tileWidth, map[0].length*tileHeight,BufferedImage.TYPE_INT_RGB);
 		Graphics g = image.createGraphics();
-		for(int i=0;i<map.length;i++){
-			for(int j=0;j<map[i].length;j++){
-				g.drawImage(map[i][j].getImage(), i*tileWidth, j*tileHeight, null);
+		for(int rows=0;rows<map.length;rows++){
+			for(int cols=0;cols<map[rows].length;cols++){
+				g.drawImage(map[rows][cols].getImage(), rows*tileWidth, cols*tileHeight, null);
 			}
 		}
 		g.dispose();
